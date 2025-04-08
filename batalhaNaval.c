a9c643d5571a7dbde94c98071199841e70ed0576
Carlos Tupinambá <calmir281@gmail.com>
2025-04-08 13:13:47 +0000

    conclui o desafio aventureiro tema 4 batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+#define linhas 10
+#define colunas 10
+
 
 void idcoluna(){
 
         // Função para imprimir as letras das colunas (A a J)
-     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; 
+     char coluna[colunas] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; 
 
         printf("   ");
         for (int i = 0; i < 10; i++)
@@ -22,39 +25,64 @@ void idcoluna(){
     void idtabuleiro(){ //recursividade para organizar o codigo
 
          const int tamanhotabuleiro = 10;
-         int tabuleiro[10][10];
+         int tabuleiro[linhas][colunas];
+         int soma;
         
             for (int i = 0; i < tamanhotabuleiro; i++){
                  // Imprime o tabuleiro com os números das linhas
                  printf("%2d|", i + 1);
 
-                for ( int j = 0; j < tamanhotabuleiro; j++)
-             { 
+                for ( int j = 0; j < tamanhotabuleiro; j++){ 
                 tabuleiro[i][j] = 0;
-                
+                 soma = i + j ;
+
 
                  // Posiciona os "navios" no tabuleiro com o valor 1
-                tabuleiro[1][2] = 1; tabuleiro[1][3] = 1; tabuleiro[1][4] = 1;
-                tabuleiro[5][2] = 1; tabuleiro[6][2] = 1; tabuleiro[7][2] = 1;
-                tabuleiro[3][7] = 1; tabuleiro[2][8] = 1; tabuleiro[1][9] = 1;
+                tabuleiro[5][1] = 3; tabuleiro[5][2] = 3; tabuleiro[5][3] = 3;
+                tabuleiro[9][7] = 3; tabuleiro[9][8] = 3; tabuleiro[9][9] = 3;
+                
 
+
+                // função para imprimir o navio na diagonal (tamanho 3)
+                    for (int i = 0; i <= 2; i++)
+                        {
+                        
+                            if (soma == 9)
+                            {
+                                tabuleiro[i][j] = 3;
+
+                            }else if(soma == 3)
+                            {
+                                tabuleiro[i][j] = 3;
+                            }
+                            else{
+                                tabuleiro[i][j] = 0;
+                            }
+                        }
+
+
+
+                
 
                  // Imprime os valores 0 ou 1
                  printf("%2d", tabuleiro[i][j]);
-             }
-                 printf("\n");
+                }
+
+                
+                
+                printf("\n");
             }
 
-        }
+    }
 
    
 int main(){
 
-       printf("--------------------------------------------\n");
-       printf("\n");
-       printf("BEM VINDO AO BATALHA NAVAL\n");
-       printf("\n");
-    
+        printf("--------------------------------------------\n");
+        printf("\n");
+        printf("BEM VINDO AO BATALHA NAVAL\n");
+        printf("\n");
+
 
 
          idcoluna();
@@ -63,9 +91,9 @@ int main(){
     
         
         
-         printf("--------------------------------------------");
+        printf("--------------------------------------------");
 
 
 
     return 0;
-}
+}   
